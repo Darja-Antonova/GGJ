135879d5b286b4ff3c4e8d46d09767c8ec61717e
Darja-Antonova <N00253685@iadt.ie>
2026-01-31 12:27:20 +0000

    Buttons and Text

--- Assets/Scripts/Scene01/Scene01Events.cs
+++ Assets/Scripts/Scene01/Scene01Events.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Scene01Events : MonoBehaviour
@@ -6,6 +7,23 @@ public class Scene01Events : MonoBehaviour
     public GameObject fadeScreenIn;
     public GameObject charHolly;
     public GameObject charJun;
+    public GameObject textBox;
+
+    [SerializeField] AudioSource Pop;
+
+    [SerializeField] string textToSpeak;
+    [SerializeField] int currentTextLength;
+    [SerializeField] int textLength;
+    [SerializeField] GameObject mainTextObject;
+
+    [SerializeField] GameObject nextButton;
+    [SerializeField] int eventPos = 0;
+    [SerializeField] GameObject charName;
+
+    void Update()
+    {
+        textLength = TextCreator.charCount;
+    }
 
     private void Start()
     {
@@ -14,12 +32,56 @@ public class Scene01Events : MonoBehaviour
 
     IEnumerator EventStarter()
     {
+        //Event 0
         yield return new WaitForSeconds(2);
         fadeScreenIn.SetActive(false);
         charHolly.SetActive(true);
         yield return new WaitForSeconds(2);
         //Text function for future
-        yield return new WaitForSeconds(2);
-        charJun.SetActive(true);
+        mainTextObject.SetActive(true);
+        textToSpeak = "Die Asshole.";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+        Pop.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+        nextButton.SetActive(true);
+        eventPos = 1;
+
+    }
+
+
+IEnumerator EventOne() 
+{
+    //Event 1
+    nextButton.SetActive(false);
+    charJun.SetActive(true);
+    textBox.SetActive(true);
+    charName.GetComponent<TMPro.TMP_Text>().text = "Jun Char";
+
+    textToSpeak = "Oh, you like My Chemical Romance?, Name five songs.";
+    textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+    currentTextLength = textToSpeak.Length;
+    TextCreator.runTextPrint = true;
+    yield return new WaitForSeconds(0.05f);
+    yield return new WaitForSeconds(1);
+    yield return new WaitUntil(() => textLength == currentTextLength);
+    yield return new WaitForSeconds(0.5f);
+
+    Pop.Play();
+    nextButton.SetActive(true);
+    eventPos = 2;
+}
+
+
+    public void NextButton()
+    {
+if (eventPos == 1) 
+      {
+          StartCoroutine(EventOne());
+      }
     }
 }
